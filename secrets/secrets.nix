bbd8d6c82f842b185a5ef7a5a03df91e94d55f02
yolofanhd <76175017+yolofanhd@users.noreply.github.com>
2024-09-22 01:12:37 +0200

    feat: added agenix configuration
    Signed-off-by: yolofanhd <76175017+yolofanhd@users.noreply.github.com>
    
    refactor: outsourced agenix configuration into own module
    
    Signed-off-by: yolofanhd <76175017+yolofanhd@users.noreply.github.com>
    
    refactor: added new secrets
    
    fix: added user permissions

---
+++ secrets/secrets.nix
@@ -0,0 +1,21 @@
+let
+  fractalix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGT/Y1zgcuhNC1iWFyZX2p/6vyHsnm4z/6g6dzJ8wyjI";
+  vectorix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDIRGcNK8dw90AC9xXwzRDDe29uhKD+RhWn1rxvV831y";
+  users = [ fractalix vectorix ];
+
+  arithmancer = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOS9kdQWuA+RbbF3LKhxYvcqjud8lU+AJg73lnZQMXR3";
+  spinorer = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJRtKm8rjoGQkEl6KLNG7uq6KnoQrcrJEWAjnKRR5Qtp";
+  systems = [ arithmancer spinorer ];
+in
+{
+  "test.age".publicKeys = users ++ systems;
+
+  "github-ssh.age".publicKeys = users ++ systems;
+  "github-ssh.pub.age".publicKeys = users ++ systems;
+
+  "oracle-ssh.age".publicKeys = users ++ systems;
+  "oracle-ssh.pub.age".publicKeys = users ++ systems;
+
+  "rpi5ssh.age".publicKeys = users ++ systems;
+  "rpi4ssh.age".publicKeys = users ++ systems;
+}
