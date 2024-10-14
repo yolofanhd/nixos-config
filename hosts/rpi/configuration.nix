fa2f54ccd29063404568e452516986473eb1c90e
yolofanhd <76175017+yolofanhd@users.noreply.github.com>
2024-10-14 18:52:17 +0200

    feat: added raspi4 and merged rpi4 and rpi5 config
    Signed-off-by: yolofanhd <76175017+yolofanhd@users.noreply.github.com>

--- hosts/rpi/configuration.nix
+++ hosts/rpi/configuration.nix
@@ -5,6 +5,7 @@
 , system
 , hostName
 , includeHardwareConfig
+, isPi5
 , ...
 }:
 let
@@ -17,35 +18,35 @@ in
       [
         (rootPrefix + /hardware-configuration.nix)
       ]
+    ++ lib.optionals isPi5
+      [
+        ./pi5.nix
+      ]
     ++ [
       (modulePrefix + /nix-defaults.nix)
       inputs.home-manager.nixosModules.default
     ];
-
   environment.systemPackages = with pkgs; [
     vim
     git
     htop
     btop
     wget
-    inputs.myvim.packages."${system}".default
+    libraspberrypi
+    raspberrypi-eeprom
   ];
 
   users = {
     users.pi = {
       isNormalUser = true;
       extraGroups = [ "wheel" ];
-      packages = with pkgs; [
-      ];
     };
   };
-
   boot = {
-    loader.systemd-boot.enable = true;
-    loader.efi.canTouchEfiVariables = false;
-    kernelPackages = inputs.rpi5-flake.legacyPackages.aarch64-linux.linuxPackages_rpi5;
+    loader = {
+      efi.canTouchEfiVariables = false;
+    };
   };
-
   networking = {
     inherit hostName;
     networkmanager.enable = true;
@@ -66,7 +67,6 @@ in
       ${username} = import ./home.nix;
     };
   };
-
   services.openssh.enable = true;
   system.stateVersion = "24.11"; #WARN: Do NOT! edit!!
 }
