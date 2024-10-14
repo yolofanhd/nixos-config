c6942e04156fbace9f9c5695f47cbfa8bb289bfe
yolofanhd <76175017+yolofanhd@users.noreply.github.com>
2024-10-14 19:36:29 +0200

    feat: init k3s

--- hosts/rpi/configuration.nix
+++ hosts/rpi/configuration.nix
@@ -49,6 +49,12 @@ in
     };
   };
   boot = {
+    kernelParams = [
+      "cgroup_enable=cpuset"
+      "cgroup_memory=1"
+      "cgroup_enable=memory"
+    ];
+
     loader = {
       efi.canTouchEfiVariables = false;
     };
