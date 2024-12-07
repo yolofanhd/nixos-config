c9bc984774816bbdb137d7d3b3d5fbc0a6559d8e
yolofanhd <76175017+yolofanhd@users.noreply.github.com>
2024-12-07 23:03:26 +0100

    feat: edit

--- hosts/arithmancer/configuration.nix
+++ hosts/arithmancer/configuration.nix
@@ -33,7 +33,26 @@ in
       inputs.home-manager.nixosModules.default
     ];
 
+  hardware.opengl.driSupport32Bit = true;
+
+  # configuration.nix
+
+  boot.kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
+
+  hardware.nvidia.powerManagement.enable = true;
+
+  # Making sure to use the proprietary drivers until the issue above is fixed upstream
+  hardware.nvidia.open = false;
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   environment.systemPackages = with pkgs; [
+    prusa-slicer
     btop
     cmake
     docker
@@ -51,7 +70,37 @@ in
     wget
     inputs.agenix.packages.${system}.default
     (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    (
+      let
+        cura5 = appimageTools.wrapType2 rec {
+          name = "cura5";
+          version = "5.4.0";
+          src = fetchurl {
+            url = "https://github.com/Ultimaker/Cura/releases/download/${version}/UltiMaker-Cura-${version}-linux-modern.AppImage";
+            hash = "sha256-QVv7Wkfo082PH6n6rpsB79st2xK2+Np9ivBg/PYZd74=";
+          };
+          extraPkgs = pkgs: with pkgs; [ ];
+        };
+      in
+      writeScriptBin "cura" ''
+        #! ${pkgs.bash}/bin/bash
+        # AppImage version of Cura loses current working directory and treats all paths relateive to $HOME.
+        # So we convert each of the files passed as argument to an absolute path.
+        # This fixes use cases like `cd /path/to/my/files; cura mymodel.stl anothermodel.stl`.
+        args=()
+        for a in "$@"; do
+          if [ -e "$a" ]; then
+            a="$(realpath "$a")"
+          fi
+          args+=("$a")
+        done
+        exec "${cura5}/bin/cura5" "''${args[@]}"
+      ''
+    )
   ];
+
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
   users.users.${username} = {
     isNormalUser = true;
@@ -60,7 +109,7 @@ in
     initialPassword = "nixos";
     packages = with pkgs; [
       anki-bin
-      bitwarden
+      #bitwarden
       blender
       cheat
       discord
