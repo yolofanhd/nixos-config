f7f3261a22ded5a7429c1e6193eed10d02ef6483
yolofanhd <76175017+yolofanhd@users.noreply.github.com>
2024-12-08 17:38:30 +0100

    feat: added agenix secret for kubernetes cluster token

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,20 +1,15 @@
 let
   fractalix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGT/Y1zgcuhNC1iWFyZX2p/6vyHsnm4z/6g6dzJ8wyjI";
   vectorix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDIRGcNK8dw90AC9xXwzRDDe29uhKD+RhWn1rxvV831y";
-  users = [ fractalix vectorix ];
+  pi4 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID5n8GP9bJ0RBh3muDqX4kEgTRGo7OI43J0d5V0pjW0e";
+  pi5 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKsFU9SsmH2OAoDZNOUWqWGwQwNH6uwaPzObhaDP23hL";
+  users = [ fractalix vectorix pi4 pi5 ];
 
   arithmancer = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOS9kdQWuA+RbbF3LKhxYvcqjud8lU+AJg73lnZQMXR3";
   spinorer = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJRtKm8rjoGQkEl6KLNG7uq6KnoQrcrJEWAjnKRR5Qtp";
-  systems = [ arithmancer spinorer ];
-
-  pi4 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID5n8GP9bJ0RBh3muDqX4kEgTRGo7OI43J0d5V0pjW0e";
-  pi5 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKsFU9SsmH2OAoDZNOUWqWGwQwNH6uwaPzObhaDP23hL";
-
   pi4system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO+CU5raZ+vwE6Nv2f8Y10/wG92gviik7LS2L6R9h8n/";
   pi5system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK1rlhG3imQL6jfzzEPZyX5T8eWku66b14gk6C96QFYW";
-
-  k3s_users = [ pi4 pi5 ];
-  k3s_systems = [ pi4system pi5system ];
+  systems = [ arithmancer spinorer pi4system pi5system ];
 in
 {
   "test.age".publicKeys = users ++ systems;
@@ -28,5 +23,5 @@ in
   "rpi5ssh.age".publicKeys = users ++ systems;
   "rpi4ssh.age".publicKeys = users ++ systems;
 
-  "kubernetes.age".publicKeys = k3s_users ++ k3s_systems ++ users ++ systems;
+  "kubernetes.age".publicKeys = users ++ systems;
 }
